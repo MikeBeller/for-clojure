8403485cd97463f1bce28b9190db637ea663f120
Mike Beller <MikeBeller@users.noreply.github.com>
2015-05-09 14:13:14 -0400

    Correct solution for problem 91

--- graph_connectivity.clj
+++ graph_connectivity.clj
@@ -1,18 +1,22 @@
 (defn is-conn [edges]
-  (let [ all-nodes (set (flatten edges))]
+  (let [ all-nodes (set (flatten (seq edges)))]
     (loop [ visited #{}
             to-visit #{(ffirst edges)} ]
-      (let [ [cur & toseq] (seq to-visit)
-             visited (conj visited cur)]
-        (if-not to-visit
-          (= visited all-nodes)
-          (recur
-           visited
-           (into (set toseq)
-                 (filter (comp not visited)
-                         (for [[a,b] edges
-                               :when (or (= cur a) (= cur b)) ]
-                           (if (= cur a) b a))))))))))
+      (if (empty? to-visit)
+        (= visited all-nodes)
+        (let [ [cur & toseq] (seq to-visit)
+               visited (conj visited cur)]
+          (recur visited
+                 (into (set toseq)
+                       (filter (comp not visited)
+                               (for [[a,b] edges
+                                     :when (or (= cur a) (= cur b)) ]
+                                 (if (= cur a) b a))))))))))
 
+(= true (is-conn #{[:a :a]}))
 
  (= true (is-conn [[1 2] [2 3] [3 4] [4 1]]))
+
+ (= false (is-conn #{[:a :b] [:b :c] [:c :d]
+               [:x :y] [:d :a] [:b :e]}))
+
