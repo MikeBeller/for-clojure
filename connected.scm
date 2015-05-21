e1d1796ae493f9d7eb5bf20170c15eb25faa20e3
Mike Beller <MikeBeller@users.noreply.github.com>
2015-05-21 08:23:57 -0400

    Beginnings of a more efficient version of graph connected, based on a node->nodes data structure.  Not working yet.

--- connected.scm
+++ connected.scm
@@ -23,6 +23,20 @@
            (loop (remove e es) (cons e seen))
            #f)))))
 
+; need to still write 'normalize' - change graph from list of edges
+;  to a assoc with car equal node name, cdr equal list of connected nodes
+; need to write set-extend, which adds items in second arg to list in
+;  first arg, only for items not already in the list
+;(define graphcon (edges)
+;  (let ((gr (normalize edges)))
+;    (let loop ((todo (list (caar gr))) (tosee (map car gr)))
+;      (if (null? todo) (null? tosee)
+;        (let ((node (car todo)))
+;          ; if node still to be seen, add its connected nodes to "todo"
+;          (if (member node tosee)
+;            (loop ((set-extend todo (cdr node)) (remove (car node) tosee)))))))))
+
+
 (test (gr-connected '((a . a))))
 (test (gr-connected '((a . b))))
 (test (not (gr-connected '((1 . 2) (2 . 3) (3 . 1) (4 . 5) (5 . 6) (6 . 4)))))
